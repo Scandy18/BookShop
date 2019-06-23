caf85da0d9f75ce694bc7191c8b8e57f99bd4400
Scandy18 <406900308@qq.com>
2019-06-23 15:15:36 +0800

    数据库连接

---
+++ BookShop/BookShop/Models/DataMaintain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookShop.Models
+{
+    public class DataMaintain : DbContext
+    {
+        public DataMaintain() : base ("name=DefaultConnection")
+        {
+            //Database.SetInitializer<DataMaintain>(null);
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ BookShop/BookShop/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShop.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Please enter the Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Please enter the correct format")]
+        public string Email { get; set; }
+
+        [DisplayName("NickName")]
+        [Required(ErrorMessage = "Please enter the NickName")]
+        [MaxLength(20, ErrorMessage = "MaxLength 20 words")]
+        [MinLength(6, ErrorMessage = "MinLength 6 words")]
+        public string NickName { get; set; }
+
+        [DisplayName("Password")]
+        [Required(ErrorMessage = "Please enter the Password")]
+        [MaxLength(10, ErrorMessage = "MaxLength 10 words")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        
+    }
+}

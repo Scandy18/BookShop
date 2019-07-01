285b1c37280aa50ed5b908c7520f1c14fe8ad1da
Scandy18 <406900308@qq.com>
2019-07-01 23:27:50 +0800

    消息系统

---
+++ BookShop/BookShop/Controllers/MessagesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookShop.Models;
+
+namespace BookShop.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private DataMaintain db = new DataMaintain();
+        // GET: Messages
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Receive()
+        {
+            return View();
+        }
+
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send([Bind(Include = "Id,Sender,Receiver,Content,Time")] Message msg)
+        {
+            if (msg == null)
+            {
+                return HttpNotFound();
+            }
+            msg.Sender = int.Parse(SessionHelper.Get("id").ToString());
+            msg.Time = DateTime.Now;
+            db.Message.Add(msg);
+            db.SaveChanges();
+            return RedirectToAction("Receive");
+        }
+
+    }
+}
